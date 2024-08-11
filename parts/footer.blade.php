8fc3cb855a19a7a71058121165e73777ed268f53
Ikennaf1 <iiikbros202@gmail.com>
2024-08-11 19:03:23 +0100

    The social icons

--- parts/footer.blade.php
+++ parts/footer.blade.php
@@ -1,3 +1,7 @@
+<?php
+$socials = themeGetSocials();
+?>
+
 <div class="bg-black">
     <div class="body-margin">
         <div class="contact-container">
@@ -5,13 +9,16 @@
             <div class="py-5">
                 <span class="text-base font-bold tracking-wide text-white">Social platforms</span>
                 <div class="flex items-center flex-wrap mt-1 space-x-5">
-                    <a href="/" class="text-white transition-colors duration-300">
-                        <svg viewBox="0 0 24 24" fill="currentColor" class="h-5">
-                            <path
-                                d="M24,4.6c-0.9,0.4-1.8,0.7-2.8,0.8c1-0.6,1.8-1.6,2.2-2.7c-1,0.6-2,1-3.1,1.2c-0.9-1-2.2-1.6-3.6-1.6 c-2.7,0-4.9,2.2-4.9,4.9c0,0.4,0,0.8,0.1,1.1C7.7,8.1,4.1,6.1,1.7,3.1C1.2,3.9,1,4.7,1,5.6c0,1.7,0.9,3.2,2.2,4.1 C2.4,9.7,1.6,9.5,1,9.1c0,0,0,0,0,0.1c0,2.4,1.7,4.4,3.9,4.8c-0.4,0.1-0.8,0.2-1.3,0.2c-0.3,0-0.6,0-0.9-0.1c0.6,2,2.4,3.4,4.6,3.4 c-1.7,1.3-3.8,2.1-6.1,2.1c-0.4,0-0.8,0-1.2-0.1c2.2,1.4,4.8,2.2,7.5,2.2c9.1,0,14-7.5,14-14c0-0.2,0-0.4,0-0.6 C22.5,6.4,23.3,5.5,24,4.6z">
-                            </path>
-                        </svg>
+                    @foreach ($socials as $social => $details)
+                    @php
+                        $socialUrl = settings('r', 'akkura.'.$social);
+                    @endphp
+                    @if (!empty($socialUrl))
+                    <a href="{{$socialUrl}}" rel="nofollow noopener noreferrer" class="text-white transition-colors duration-300">
+                        <span>&nbsp;&nbsp;&nbsp;&nbsp;{!! $details['icon'] !!}&nbsp;&nbsp;&nbsp;&nbsp;</span>
                     </a>
+                    @endif
+                    @endforeach
                 </div>
                 <p class="mt-2 text-sm text-gray-500">
                     Please Follow
